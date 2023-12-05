f6f80ddf14fc948074d21a1cdb68858e7514d735
Skyler Hwang <50127023+poohSky@users.noreply.github.com>
2023-12-06 00:03:42 +0900

    Add MFC repository

---
+++ MFCApplication1/gtest/test.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+
+TEST(TestCaseName, TestName1) {
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+}
+TEST(TestCaseName, TestName2) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+}
