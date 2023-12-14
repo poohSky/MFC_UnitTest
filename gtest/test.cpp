75598c9dd50da6703f6a78c8c85c15522f45b557
Skyler Hwang <50127023+poohSky@users.noreply.github.com>
2023-12-15 00:03:50 +0900

    Update Path

---
+++ gtest/test.cpp
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "../pch.h"
+
+TEST(TestCaseName, TestName1) {
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+}
+TEST(TestCaseName, TestName2) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+}
